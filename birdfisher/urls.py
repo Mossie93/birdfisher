0b4cef7f36cd6d3588f9f95d626b30687ef51acc
Rafal Mossakowski <rmossakowski@gmail.com>
2020-03-31 21:40:01 +0200

    Add auth urls

--- birdfisher/urls.py
+++ birdfisher/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib import auth
 from django.urls import include, path
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('', include('django.contrib.auth.urls')),
 ]
